8667e8654fb1932ae555ef447610ec6eef3f5ea4
Abhay Kumar Ray <abhayray2002@gmail.com>
2021-10-30 14:46:44 +0530

    Co-authored-by: Shardul Kurdukar <24shardul@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import Paraphrase from './components/paraphrase/Paraphrase';
 import Grammer from './components/Grammer/Grammer';
+import Summariser from './components/Summariser/Summariser';
 import Page404 from './components/Page404/Page404';
 import Nav from './components/Nav/Nav';
 import {BrowserRouter as Router, Switch, Route, Link, useLocation } from "react-router-dom";
@@ -17,21 +18,28 @@ function App() {
             <Nav/>  
             <Home />
           </Route> 
+
           <Route exact path="/paraphrase">
           <Nav/>  
-
             <Paraphrase />
           </Route>
+
           <Route exact path="/grammer">
           <Nav/>  
-
             <Grammer />
           </Route>
+
+          <Route exact path="/Summariser">
+          <Nav/>  
+            <Summariser />
+          </Route>
+
           <Route exact path="/about">
           <Nav/>  
-            
             <Grammer />
           </Route>
+
+          
           <Route exact path="*">
             <Page404 />
           </Route>
